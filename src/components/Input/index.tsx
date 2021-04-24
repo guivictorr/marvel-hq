3f4ea8ea5dde2b67ea66c72440e2ff28463e7ad2
guivictorr <guilhermevictor20@gmail.com>
2021-04-24 02:49:12 -0300

    feat(input): created input component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,25 @@
+import { InputHTMLAttributes } from 'react';
+import Container from './styles';
+
+type InputProps = InputHTMLAttributes<HTMLInputElement> & {
+  btnAction(any: unknown): unknown;
+};
+
+const Input = ({ btnAction, value, onChange, onKeyPress }: InputProps) => {
+  return (
+    <Container>
+      <input
+        type="text"
+        placeholder="Procure por quadrinhos"
+        value={value}
+        onChange={onChange}
+        onKeyPress={onKeyPress}
+      />
+      <button onClick={btnAction}>
+        <img src="search.svg" alt="Procurar" />
+      </button>
+    </Container>
+  );
+};
+
+export default Input;
