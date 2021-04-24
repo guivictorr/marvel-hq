7b19a9f39b143637f401d4c61c2a5de887de8116
guivictorr <guilhermevictor20@gmail.com>
2021-04-24 02:50:21 -0300

    feat(wrapper): created wrapper component

---
+++ src/components/Wrapper/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+type WrapperProps = {
+  maxWidth: number;
+};
+
+const Wrapper = styled.div<WrapperProps>`
+  max-width: ${({ maxWidth }) => maxWidth}rem;
+  margin: 0 auto;
+  padding: 0 20px;
+  height: 100%;
+`;
+
+export default Wrapper;
