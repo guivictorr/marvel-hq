8dbd3117d5c7ded6ff30604d2759d1da9b75d4ea
guivictorr <guilhermevictor20@gmail.com>
2021-04-24 02:51:22 -0300

    feat(home): created home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,71 @@
-export default function Home() {
-  return <h1>Hello World</h1>;
-}
+import { ChangeEvent, Fragment, KeyboardEvent, useState } from 'react';
+
+import useComics from '../hooks/useComics';
+
+import Card from '../components/Card';
+import Error from '../components/Error/styles';
+import Grid from '../components/Grid/styles';
+import Header from '../components/Header';
+import Wrapper from '../components/Wrapper/styles';
+import Input from '../components/Input';
+import Button from '../components/Button';
+import Details from '../components/Details';
+
+import * as F from '../styles/pages/home';
+import { useModal } from '../context/modalContext';
+
+const Home = () => {
+  const { comics, loadMore, fetchComics, error } = useComics();
+  const { modalIsOpen } = useModal();
+  const [inputText, setInputText] = useState('');
+
+  const getInputText = (event: ChangeEvent<HTMLInputElement>) => {
+    setInputText(event.target.value);
+  };
+
+  const searchOnKeyPress = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') getDataByInput(inputText);
+  };
+
+  const getDataByInput = async (name: string) => {
+    await fetchComics(name);
+    setInputText('');
+  };
+
+  return (
+    <F.Container>
+      <Header />
+      <Wrapper maxWidth={160}>
+        <F.Content>
+          <Input
+            btnAction={getDataByInput}
+            onChange={getInputText}
+            value={inputText}
+            onKeyPress={searchOnKeyPress}
+          />
+
+          <Error visible={Boolean(error)}>{error}</Error>
+
+          <Grid columns={6}>
+            {comics.results ? (
+              comics.results.map(comic => <Card key={comic.id} comic={comic} />)
+            ) : (
+              <Fragment />
+            )}
+          </Grid>
+
+          <Wrapper maxWidth={50}>
+            <Button
+              title="Carregar mais"
+              visible={comics.count >= 1 && comics.count < 90}
+              onClick={loadMore}
+            />
+          </Wrapper>
+        </F.Content>
+      </Wrapper>
+      <Details visible={modalIsOpen} />
+    </F.Container>
+  );
+};
+
+export default Home;
---
+++ src/styles/pages/home.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import Wrapper from '../../components/Wrapper/styles';
+
+export const Container = styled.div``;
+
+export const Content = styled.main`
+  ${Wrapper} {
+    margin-top: 20px;
+    margin-bottom: 20px;
+  }
+`;
