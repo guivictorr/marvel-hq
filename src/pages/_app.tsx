70daf8c821b5e25c9dbb155320e4c5842ba9c50a
guivictorr <guilhermevictor20@gmail.com>
2021-04-24 02:51:48 -0300

    feat(context): added Contexts providers

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,20 @@
 import { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
-
+import ModalProvider from '../context/modalContext';
+import SelectedComicsProvider from '../context/selectedComicsContext';
 import GlobalStyles from '../styles/global';
+
 import theme from '../styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Component {...pageProps} />
+      <SelectedComicsProvider>
+        <ModalProvider>
+          <Component {...pageProps} />
+        </ModalProvider>
+      </SelectedComicsProvider>
     </ThemeProvider>
   );
 }
